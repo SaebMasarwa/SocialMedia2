892c0efe757535a85e976aaf165e373bbebd3f09
Saeb Masarwa <saeb2312@gmail.com>
2024-10-16 21:17:24 +0300

    modified:   components/ProfileTabContent/ProfileTabContent.js 	modified:   components/ProfileTabContent/style.js 	modified:   navigation/MainNavigation.js

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -5,34 +5,13 @@ import Home from '../screens/Home/Home';
 import Profile from '../screens/Profile/Profile';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import {Text, View} from 'react-native';
+
 import ProfileTabTitle from '../components/ProfileTabTitle/ProfileTabTitle';
+import ProfileTabContent from '../components/ProfileTabContent/ProfileTabContent';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 const ProfileTabs = createMaterialTopTabNavigator();
-
-const Tab1 = () => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>This is tab 1</Text>
-    </View>
-  );
-};
-const Tab2 = () => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>This is tab 2</Text>
-    </View>
-  );
-};
-const Tab3 = () => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>This is tab 3</Text>
-    </View>
-  );
-};
 
 export const ProfileTabsNavigation = () => {
   return (
@@ -53,7 +32,7 @@ export const ProfileTabsNavigation = () => {
             <ProfileTabTitle isFocused={focused} title={'Photos'} />
           ),
         }}
-        component={Tab1}
+        component={ProfileTabContent}
       />
       <ProfileTabs.Screen
         name={'Tab2'}
@@ -62,7 +41,7 @@ export const ProfileTabsNavigation = () => {
             <ProfileTabTitle isFocused={focused} title={'Videos'} />
           ),
         }}
-        component={Tab2}
+        component={ProfileTabContent}
       />
       <ProfileTabs.Screen
         name={'Tab3'}
@@ -71,7 +50,7 @@ export const ProfileTabsNavigation = () => {
             <ProfileTabTitle isFocused={focused} title={'Saved'} />
           ),
         }}
-        component={Tab3}
+        component={ProfileTabContent}
       />
     </ProfileTabs.Navigator>
   );
